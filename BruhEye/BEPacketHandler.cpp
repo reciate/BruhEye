f75d67e75ce6a2780199518abeef26ce103e073d
AnusReaper <49763556+AnusReaper@users.noreply.github.com>
2020-09-15 12:05:52 +0100

    Packet handler now correctly replies to packets

--- BruhEye/BEPacketHandler.cpp
+++ BruhEye/BEPacketHandler.cpp
@@ -1,21 +1,29 @@
 #include "BEPacketHandler.h"
 
 void handlePacket(BEServer::Packet* packet) {
-	switch (packet->headerID) {
-		case (BEServer::Packet::HeaderID::Init): {
+	switch (packet->header.id) {
+		case (BEServer::Packet::Header::ID::Init): {
 			std::cout << "Init" << '\n';
+			unsigned char response[]{0, 5};
+			BEFunctions::GameData.SendPacket(reinterpret_cast<int*>(&response), sizeof(response));
 			break;
 		}
-		case (BEServer::Packet::HeaderID::Start): {
+		case (BEServer::Packet::Header::ID::Start): {
 			std::cout << "Start" << '\n';
+			unsigned char response[]{ 2, 0 };
+			BEFunctions::GameData.SendPacket(reinterpret_cast<int*>(&response), sizeof(response));
 			break;
 		}
-		case (BEServer::Packet::HeaderID::Request): {
+		case (BEServer::Packet::Header::ID::Request): {
 			std::cout << "Request" << '\n';
+			unsigned char response[]{ 4, 1 };
+			BEFunctions::GameData.SendPacket(reinterpret_cast<int*>(&response), sizeof(response));
 			break;
 		}
-		case (BEServer::Packet::HeaderID::Heartbeat): {
+		case (BEServer::Packet::Header::ID::Heartbeat): {
 			std::cout << "Heartbeat" << '\n';
+			unsigned char response[]{ 9, packet->header.sequence, 0 };
+			BEFunctions::GameData.SendPacket(reinterpret_cast<int*>(&response), sizeof(response));
 			break;
 		}
 	}
